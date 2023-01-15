46a4f857fd2c6807243faac36e9cf18b34cf8ed9
qpwoeirut <stanleyzhong8@gmail.com>
2023-01-15 13:26:37 -0800

    Ignore errors from old version

--- battlecode23-scaffold-main/src/qp_carrierlauncherspam_3/RobotPlayer.java
+++ battlecode23-scaffold-main/src/qp_carrierlauncherspam_3/RobotPlayer.java
@@ -33,11 +33,11 @@ public strictfp class RobotPlayer {
             try {
                 bot.processRound();
             } catch (GameActionException e) {
-                System.out.println("GameActionException: " + rc.getType());
-                e.printStackTrace();
+//                System.out.println("GameActionException: " + rc.getType());
+//                e.printStackTrace();
             } catch (Exception e) {
-                System.out.println("Exception: " + rc.getType());
-                e.printStackTrace();
+//                System.out.println("Exception: " + rc.getType());
+//                e.printStackTrace();
             } finally {  // end turn
 //                if (startingRound != rc.getRoundNum()) {
 //                    System.out.println("Started on round " + startingRound + " but ended on round " + rc.getRoundNum() + " with " + Clock.getBytecodeNum() + " bytecode used");
