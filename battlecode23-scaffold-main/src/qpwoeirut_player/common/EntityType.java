afe27cf00c0d6233156bdb617862462f37169f92
qpwoeirut <stanleyzhong8@gmail.com>
2023-01-15 18:32:21 -0800

    Get rid of island comms for now

--- battlecode23-scaffold-main/src/qpwoeirut_player/common/EntityType.java
+++ battlecode23-scaffold-main/src/qpwoeirut_player/common/EntityType.java
@@ -1,9 +1,8 @@
 package qpwoeirut_player.common;
 
 public enum EntityType {
-    WELL(0, 16, 20, 200, 12, 3),
-    ENEMY(16, 36, 1, 1, 1, 1),
-    ISLAND(52, 8, 1, 1, 1, 1),
+    WELL(0, 20, 20, 200, 12, 3),
+    ENEMY(20, 40, 1, 1, 1, 1),
     HQ(60, 4, 50, 100, 36, 4);
 
     public final int offset, count;
