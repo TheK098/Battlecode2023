88d2e0b34592ee4c3378e809ce1dbf64fa69faf6
qpwoeirut <stanleyzhong8@gmail.com>
2023-01-16 19:08:13 -0800

    Bytecode-optimize IntHashMap

--- battlecode23-scaffold-main/src/qp1/utilities/IntHashMap.java
+++ battlecode23-scaffold-main/src/qp1/utilities/IntHashMap.java
@@ -1,7 +1,6 @@
 package qp1.utilities;
 
 import java.util.Arrays;
-import java.util.Iterator;
 
 // Based on https://github.com/skittles1412/Battlecode2021/blob/master/src/utilities/IntHashMap.java
 
@@ -9,15 +8,15 @@ import java.util.Iterator;
  * An int -> int hashmap
  * 0 is the default value for nonexistent keys
  */
-public class IntHashMap implements Iterable<IntHashMap.Entry> {
+public class IntHashMap {
     private static final int INITIAL_BIN_CAPACITY = 4;
     private final int n;
 //    private int size;
     private final int[] ind;
     private final int[][] keys, values;
 
-    public IntHashMap(int initialCapacity) {
-        n = initialCapacity;
+    public IntHashMap(int bucketCount) {
+        n = bucketCount;
         ind = new int[n];
         keys = new int[n][INITIAL_BIN_CAPACITY];
         values = new int[n][INITIAL_BIN_CAPACITY];
@@ -28,31 +27,30 @@ public class IntHashMap implements Iterable<IntHashMap.Entry> {
         int cind = ind[hash];
         int[] ckeys = keys[hash];
         int[] cvalues = values[hash];
-        for (int i = 0; i < cind; i++) {
+        for (int i = cind; i --> 0;) {
             if (ckeys[i] == key) {
                 cvalues[i] = value;
                 return;
             }
         }
-//        size++;
-        int len;
-        if (cind == (len = ckeys.length)) {
-            keys[hash] = Arrays.copyOf(keys[hash], len *= 2);
-            values[hash] = Arrays.copyOf(values[hash], len);
+        if (cind == ckeys.length) {
+            keys[hash] = Arrays.copyOf(keys[hash], ckeys.length * 2);
+            values[hash] = Arrays.copyOf(values[hash], ckeys.length * 2);
             keys[hash][cind] = key;
-            values[hash][ind[hash]++] = value;
+            values[hash][cind] = value;
         } else {
             ckeys[cind] = key;
-            cvalues[ind[hash]++] = value;
+            cvalues[cind] = value;
         }
+        ++ind[hash];
+//        size++;
     }
 
     public int get(int key) {
         int hash = key % n;
-        int cind = ind[hash];
         int[] ckeys = keys[hash];
         int[] cvalues = values[hash];
-        for (int i = 0; i < cind; i++) {
+        for (int i = ind[hash]; i --> 0;) {
             if (ckeys[i] == key) {
                 return cvalues[i];
             }
@@ -82,38 +80,6 @@ public class IntHashMap implements Iterable<IntHashMap.Entry> {
             this.key = key;
             this.value = value;
         }
-    }
-
-    private class Itr implements Iterator<Entry> {
-        int binInd = 0;//index of last bin that was accessed
-        int nodeInd = -1;//index of the last node in that bin that was accessed
-
-        @Override
-        public boolean hasNext() {
-            if (binInd < n) {
-                if (nodeInd + 1 >= ind[binInd]) {
-                    while (++binInd < n && ind[binInd] == 0) ;
-                    nodeInd = -1;
-                }
-            }
-            return binInd < n;
-        }
-
-        @Override
-        public Entry next() {
-            if (binInd < n) {
-                if (++nodeInd >= ind[binInd]) {
-                    while (++binInd < n && ind[binInd] == 0) ;
-                    nodeInd = 0;
-                }
-            }
-            return new Entry(keys[binInd][nodeInd], values[binInd][nodeInd]);
-        }
-    }
-
-    @Override
-    public Iterator<Entry> iterator() {
-        return new Itr();
     }
 
 //    public int size() {
