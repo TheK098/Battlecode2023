d9d88faa1651b9462d9d7003b6115c9e15a17c26
qpwoeirut <stanleyzhong8@gmail.com>
2023-01-19 13:35:17 -0800

    Add nullplayer for testing

---
+++ src/nullplayer/RobotPlayer.java
@@ -0,0 +1,13 @@
+package nullplayer;
+
+import battlecode.common.*;
+
+
+/**
+ * This player does literally nothing. For testing purposes.
+ */
+public strictfp class RobotPlayer {
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+    }
+}
