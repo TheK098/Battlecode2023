b5972b187abb637c0d5dbb7b220c8b9a713906dc
qpwoeirut <stanleyzhong8@gmail.com>
2023-01-24 23:08:38 -0800

    Fix nullplayer

--- src/nullplayer/RobotPlayer.java
+++ src/nullplayer/RobotPlayer.java
@@ -9,5 +9,6 @@ import battlecode.common.*;
 public strictfp class RobotPlayer {
     @SuppressWarnings("unused")
     public static void run(RobotController rc) throws GameActionException {
+        while (true) Clock.yield();
     }
 }
