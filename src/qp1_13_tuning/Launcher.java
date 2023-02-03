39e1e0dd63db48fb000620bd30411612fb3e8cd6
qpwoeirut <stanleyzhong8@gmail.com>
2023-02-03 09:57:12 -0800

    Add copy of bot for more tuning

---
+++ src/qp1_13_tuning/Launcher.java
@@ -0,0 +1,216 @@
+package qp1_13_tuning;
+
+import battlecode.common.*;
+import qp1_13_tuning.communications.Comms;
+import qp1_13_tuning.communications.Comms.EnemySighting;
+import qp1_13_tuning.communications.Comms.IslandInfo;
+import qp1_13_tuning.navigation.SpreadSettings;
+import qp1_13_tuning.utilities.FastRandom;
+import qp1_13_tuning.utilities.IntHashMap;
+
+import static qp1_13_tuning.navigation.Pathfinding.moveToward;
+import static qp1_13_tuning.navigation.Pathfinding.spreadOut;
+import static qp1_13_tuning.utilities.Util.*;
+
+public class Launcher extends BaseBot {
+    private static final IntHashMap allyHealth = new IntHashMap(20);
+    private static int allyToFollow = -1;
+    private static int allyFollowTimer = 0;
+
+    public Launcher(RobotController rc) {
+        super(rc);
+    }
+
+    @Override
+    public void processRound() throws GameActionException {
+        MapLocation curLoc = rc.getLocation();
+        RobotInfo[] enemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+        MapLocation nearestHq = pickNearest(rc, Comms.getHqs(rc));
+        if (!handleCombat(enemies)) {
+            RobotInfo[] allies = rc.senseNearbyRobots(-1, rc.getTeam());
+            RobotInfo nearestEnemyHq = pickNearestHq(rc, enemies);
+            if (!supportAlly(enemies, allies, nearestEnemyHq)) {
+                if (!attackVisibleIsland()) {
+                    EnemySighting sighting = pickSighting();
+                    if (sighting != null) {
+                        tryMove(moveToward(rc, sighting.location, 700));
+                    } else if (!attackNearestIsland()) {
+                        Direction dir = spreadOut(rc,
+                                ((rc.getMapWidth() / 2f) - curLoc.x) / 5,
+                                ((rc.getMapHeight() / 2f) - curLoc.y) / 5,
+                                SpreadSettings.LAUNCHER);
+                        MapLocation newLoc = curLoc.add(dir);
+                        // maintain space for carriers
+                        if (adjacentToHeadquarters(rc, newLoc) && allies.length >= 16 && FastRandom.nextInt(8) != 0)
+                            tryMove(directionAway(rc, nearestHq));
+                        else if (adjacentToWell(rc, newLoc) && allies.length >= 16 && FastRandom.nextInt(8) != 0)
+                            if (rc.senseWell(curLoc) != null) tryMove(randomDirection(rc));
+                            else tryMove(directionAway(rc, pickNearest(rc, Comms.getKnownWells(rc)).location));
+                        else if (nearestEnemyHq != null && newLoc.isWithinDistanceSquared(nearestEnemyHq.location, 9))
+                            tryMove(directionAway(rc, nearestEnemyHq.location));
+                        else
+                            tryMove(dir);
+                    }
+                }
+            }
+        }
+
+        if (rc.isActionReady()) {  // try attacking again after moving
+            extraAttack(curLoc, nearestHq);
+        }
+
+        dieIfStuck();
+    }
+
+    private static void extraAttack(MapLocation curLoc, MapLocation nearestHq) throws GameActionException {
+        RobotInfo target = pickTarget(rc.senseNearbyRobots(16, rc.getTeam().opponent()));
+        if (target != null && rc.canAttack(target.location)) {
+            rc.attack(target.location);
+            lastMoveOrAction = rc.getRoundNum();
+        } else if (rc.senseCloud(rc.getLocation())) {
+            Direction dir = rc.getLocation().directionTo(nearestHq).opposite();
+            MapLocation targetLoc = curLoc.add(dir).add(dir).add(dir.rotateLeft());  // rotateLeft to stay within attack range
+            if (rc.canAttack(targetLoc)) rc.attack(targetLoc);
+        } else {  // attack cloud
+            MapLocation[] clouds = rc.senseNearbyCloudLocations(16);
+            int farthestDist = 0, farthestIdx = -1;
+            for (int i = clouds.length; i --> 0;) {
+                if (!curLoc.isWithinDistanceSquared(clouds[i], farthestDist)) {
+                    farthestDist = curLoc.distanceSquaredTo(clouds[i]);
+                    farthestIdx = i;
+                }
+            }
+            if (farthestIdx != -1 && rc.canAttack(clouds[farthestIdx])) rc.attack(clouds[farthestIdx]);
+        }
+    }
+
+    private static boolean handleCombat(RobotInfo[] enemies) throws GameActionException {
+        RobotInfo target = pickTarget(enemies);
+        if (target == null) target = pickNearest(rc, enemies);
+        if (target == null) return false;
+
+        MapLocation toAttack = target.location;
+        if (rc.canAttack(toAttack)) {
+            rc.attack(toAttack);
+            lastMoveOrAction = rc.getRoundNum();
+
+            // if we're already in a cloud, the enemy can probably see us anyway so just run away
+            // otherwise duck into a cloud if possible
+            tryMove(rc.senseCloud(rc.getLocation()) ? directionAway(rc, toAttack) : directionAwayOrCloud(rc, toAttack));
+        } else {
+            RobotInfo[] allies = rc.senseNearbyRobots(toAttack, 20, rc.getTeam());
+            int desperationScore = 0, allyLaunchers = 0;
+            for (int i = allies.length; i --> 0;) {
+                switch (allies[i].type) {
+                    case CARRIER:
+                        ++desperationScore;
+                        break;
+                    case HEADQUARTERS:
+                        desperationScore += 5;
+                        break;
+                    case LAUNCHER:
+                        ++allyLaunchers;
+                        break;
+                }
+            }
+
+            int enemyLaunchers = 0;
+            for (int i = enemies.length; i --> 0;) if (enemies[i].type == RobotType.LAUNCHER) ++enemyLaunchers;
+            if (allyLaunchers >= enemyLaunchers || desperationScore >= 3) {
+                Direction dir = directionTowardImmediate(rc, toAttack);
+                tryMove(dir);
+                if (rc.canAttack(toAttack)) {
+                    rc.attack(toAttack);
+                    lastMoveOrAction = rc.getRoundNum();
+                }
+            } else {
+                tryMove(directionAway(rc, toAttack));
+            }
+        }
+        return true;
+    }
+
+    private static boolean supportAlly(RobotInfo[] enemies, RobotInfo[] allies, RobotInfo nearestEnemyHq) throws GameActionException {
+        if (allyToFollow != -1 && (--allyFollowTimer == 0 || !rc.canSenseRobot(allyToFollow))) allyToFollow = -1;
+        if (allyToFollow == -1) {  // check if any allies were recently hurt
+            int added = 0, storedHealth;
+            for (int i = allies.length; i --> 0;) {
+                storedHealth = allyHealth.get(allies[i].ID);
+                if (allies[i].health < storedHealth) {  // ally took damage, go help them
+                    allyToFollow = allies[i].ID;
+                    allyFollowTimer = 8;
+                    allyHealth.put(allies[i].ID, allies[i].health);
+                } else if (storedHealth == 0 && ++added <= 10) {  // don't add too many in one round for bytecode reasons
+                    // haven't seen this ally before, record their health
+                    allyHealth.put(allies[i].ID, allies[i].health);
+                }
+            }
+        }
+
+        if (allyToFollow != -1) {  // support hurt ally
+            Direction dir = directionToward(rc, rc.senseRobot(allyToFollow).location);  // no pathing, just try to be nearby
+            if (enemies.length > 0 || nearestEnemyHq == null || !rc.getLocation().add(dir).isWithinDistanceSquared(nearestEnemyHq.location, 9))
+                tryMove(dir);
+            else tryMove(directionAway(rc, nearestEnemyHq.location));
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean attackVisibleIsland() throws GameActionException {
+        MapLocation nearestVisibleIsland = findNearestVisibleIslandLocation(rc.getTeam().opponent());
+        if (nearestVisibleIsland != null) {
+            tryMove(moveToward(rc, nearestVisibleIsland, 700));  // if we're already on island, just stay there
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean attackNearestIsland() throws GameActionException {
+        IslandInfo nearestIsland = pickNearest(rc, Comms.getIslands(rc), rc.getTeam().opponent());
+        if (nearestIsland != null && rc.getLocation().isWithinDistanceSquared(nearestIsland.location, 81)) {
+            tryMove(moveToward(rc, nearestIsland.location, 1600));
+            return true;
+        }
+        return false;
+    }
+
+    private static RobotInfo pickTarget(RobotInfo[] enemies) {
+        if (rc.isActionReady()) {
+            RobotInfo target = null;
+            for (int i = enemies.length; i --> 0;) {
+                if (canAttack(enemies[i].location) && enemies[i].type != RobotType.HEADQUARTERS)
+                    target = target == null ? enemies[i] : chooseTarget(target, enemies[i]);
+            }
+            return target;
+        }
+        return null;
+    }
+
+    private static boolean canAttack(MapLocation location) {
+        return (rc.isMovementReady() && rc.getLocation().add(directionTowardImmediate(rc, location)).isWithinDistanceSquared(location, 16)) ||
+                rc.getLocation().isWithinDistanceSquared(location, 16);
+    }
+
+    // assume both robots are within attacking range
+    private static RobotInfo chooseTarget(RobotInfo robot1, RobotInfo robot2) {
+        return (robot1.type != robot2.type) ? (robotTypePriority(robot1.type) < robotTypePriority(robot2.type) ? robot1 : robot2) :
+                (robot1.health != robot2.health) ? (robot1.health < robot2.health ? robot1 : robot2) :
+                        rc.getLocation().isWithinDistanceSquared(robot1.location, rc.getLocation().distanceSquaredTo(robot2.location)) ? robot1 : robot2;
+        // TODO: test tiebreak by distance, distance cutoff (needing to move vs not move), tiebreak by ID
+        // tiebreak by distance for now; by ID probably won't matter since each robot attacks sequentially
+    }
+
+    private static int robotTypePriority(RobotType robotType) {
+        switch (robotType) {
+            case LAUNCHER: return 0;
+            case CARRIER: return 1;
+            case AMPLIFIER: return 2;
+        }
+        return 3;
+    }
+
+    private static void dieIfStuck() {  // desperate times call for desperate measures
+        if (rc.getRoundNum() - lastMoveOrAction >= 200) rc.disintegrate();
+    }
+}
---
+++ src/qp1_13_tuning/navigation/Pathfinding.java
@@ -0,0 +1,253 @@
+package qp1_13_tuning.navigation;
+
+import battlecode.common.*;
+import qp1_13_tuning.communications.Comms;
+import qp1_13_tuning.communications.Comms.EnemySighting;
+import qp1_13_tuning.utilities.FastRandom;
+
+import java.util.Arrays;
+
+import static qp1_13_tuning.utilities.Util.*;
+
+public class Pathfinding {
+    public static Direction moveWhileStayingAdjacent(RobotController rc, MapLocation target) throws GameActionException {
+        MapLocation loc1 = rc.getLocation().add(Direction.NORTHWEST); MapInfo info1 = rc.onTheMap(loc1) ? rc.senseMapInfo(loc1) : null;
+        MapLocation loc2 = rc.getLocation().add(Direction.NORTHEAST); MapInfo info2 = rc.onTheMap(loc2) ? rc.senseMapInfo(loc2) : null;
+        MapLocation loc3 = rc.getLocation().add(Direction.SOUTHEAST); MapInfo info3 = rc.onTheMap(loc3) ? rc.senseMapInfo(loc3) : null;
+        MapLocation loc4 = rc.getLocation().add(Direction.SOUTHWEST); MapInfo info4 = rc.onTheMap(loc4) ? rc.senseMapInfo(loc4) : null;
+        MapLocation loc5 = rc.getLocation().add(Direction.NORTH);     MapInfo info5 = rc.onTheMap(loc5) ? rc.senseMapInfo(loc5) : null;
+        MapLocation loc6 = rc.getLocation().add(Direction.EAST);      MapInfo info6 = rc.onTheMap(loc6) ? rc.senseMapInfo(loc6) : null;
+        MapLocation loc7 = rc.getLocation().add(Direction.SOUTH);     MapInfo info7 = rc.onTheMap(loc7) ? rc.senseMapInfo(loc7) : null;
+        MapLocation loc8 = rc.getLocation().add(Direction.WEST);      MapInfo info8 = rc.onTheMap(loc8) ? rc.senseMapInfo(loc8) : null;
+
+        if (info1 != null && rc.canMove(Direction.NORTHWEST) && !info1.hasCloud() && adjacentOrEqual(loc1, target) && adjacentOrEqual(loc1.add(info1.getCurrentDirection()), target)) return Direction.NORTHWEST;
+        if (info2 != null && rc.canMove(Direction.NORTHEAST) && !info2.hasCloud() && adjacentOrEqual(loc2, target) && adjacentOrEqual(loc2.add(info2.getCurrentDirection()), target)) return Direction.NORTHEAST;
+        if (info3 != null && rc.canMove(Direction.SOUTHEAST) && !info3.hasCloud() && adjacentOrEqual(loc3, target) && adjacentOrEqual(loc3.add(info3.getCurrentDirection()), target)) return Direction.SOUTHEAST;
+        if (info4 != null && rc.canMove(Direction.SOUTHWEST) && !info4.hasCloud() && adjacentOrEqual(loc4, target) && adjacentOrEqual(loc4.add(info4.getCurrentDirection()), target)) return Direction.SOUTHWEST;
+        if (info5 != null && rc.canMove(Direction.NORTH)     && !info5.hasCloud() && adjacentOrEqual(loc5, target) && adjacentOrEqual(loc5.add(info5.getCurrentDirection()), target)) return Direction.NORTH;
+        if (info6 != null && rc.canMove(Direction.EAST)      && !info6.hasCloud() && adjacentOrEqual(loc6, target) && adjacentOrEqual(loc6.add(info6.getCurrentDirection()), target)) return Direction.EAST;
+        if (info7 != null && rc.canMove(Direction.SOUTH)     && !info7.hasCloud() && adjacentOrEqual(loc7, target) && adjacentOrEqual(loc7.add(info7.getCurrentDirection()), target)) return Direction.SOUTH;
+        if (info8 != null && rc.canMove(Direction.WEST)      && !info8.hasCloud() && adjacentOrEqual(loc8, target) && adjacentOrEqual(loc8.add(info8.getCurrentDirection()), target)) return Direction.WEST;
+
+        if (info1 != null && rc.canMove(Direction.NORTHWEST) && adjacentOrEqual(loc1, target) && adjacentOrEqual(loc1.add(info1.getCurrentDirection()), target)) return Direction.NORTHWEST;
+        if (info2 != null && rc.canMove(Direction.NORTHEAST) && adjacentOrEqual(loc2, target) && adjacentOrEqual(loc2.add(info2.getCurrentDirection()), target)) return Direction.NORTHEAST;
+        if (info3 != null && rc.canMove(Direction.SOUTHEAST) && adjacentOrEqual(loc3, target) && adjacentOrEqual(loc3.add(info3.getCurrentDirection()), target)) return Direction.SOUTHEAST;
+        if (info4 != null && rc.canMove(Direction.SOUTHWEST) && adjacentOrEqual(loc4, target) && adjacentOrEqual(loc4.add(info4.getCurrentDirection()), target)) return Direction.SOUTHWEST;
+        if (info5 != null && rc.canMove(Direction.NORTH)     && adjacentOrEqual(loc5, target) && adjacentOrEqual(loc5.add(info5.getCurrentDirection()), target)) return Direction.NORTH;
+        if (info6 != null && rc.canMove(Direction.EAST)      && adjacentOrEqual(loc6, target) && adjacentOrEqual(loc6.add(info6.getCurrentDirection()), target)) return Direction.EAST;
+        if (info7 != null && rc.canMove(Direction.SOUTH)     && adjacentOrEqual(loc7, target) && adjacentOrEqual(loc7.add(info7.getCurrentDirection()), target)) return Direction.SOUTH;
+        if (info8 != null && rc.canMove(Direction.WEST)      && adjacentOrEqual(loc8, target) && adjacentOrEqual(loc8.add(info8.getCurrentDirection()), target)) return Direction.WEST;
+
+        if (rc.canMove(Direction.NORTHWEST) && adjacentOrEqual(loc1, target)) return Direction.NORTHWEST;
+        if (rc.canMove(Direction.NORTHEAST) && adjacentOrEqual(loc2, target)) return Direction.NORTHEAST;
+        if (rc.canMove(Direction.SOUTHEAST) && adjacentOrEqual(loc3, target)) return Direction.SOUTHEAST;
+        if (rc.canMove(Direction.SOUTHWEST) && adjacentOrEqual(loc4, target)) return Direction.SOUTHWEST;
+        if (rc.canMove(Direction.NORTH)     && adjacentOrEqual(loc5, target)) return Direction.NORTH;
+        if (rc.canMove(Direction.EAST)      && adjacentOrEqual(loc6, target)) return Direction.EAST;
+        if (rc.canMove(Direction.SOUTH)     && adjacentOrEqual(loc7, target)) return Direction.SOUTH;
+        if (rc.canMove(Direction.WEST)      && adjacentOrEqual(loc8, target)) return Direction.WEST;
+
+        return Direction.CENTER;
+    }
+
+
+    // declare arrays once whenever possible to save bytecode
+    private static final Direction[][] startingDir = new Direction[9][9];
+    private static final int[][] distance = new int[9][9];
+
+    private static final MapLocation[] queue = new MapLocation[80];
+
+
+    // BFS, only handles passability
+    // bytecodeLimit should be at least 650
+    public static Direction moveToward(RobotController rc, MapLocation target, int bytecodeLimit) throws GameActionException {
+        Direction closestDir = directionToward(rc, target);
+        MapLocation curLoc = rc.getLocation();
+        MapLocation nextLoc = curLoc.add(closestDir);
+        if (closestDir != Direction.CENTER && directionTowardHypothetical(rc, nextLoc, target) != Direction.CENTER) {
+            return closestDir;
+        }
+        Arrays.fill(distance[0], 12960000);
+        Arrays.fill(distance[1], 12960000);
+        Arrays.fill(distance[2], 12960000);
+        Arrays.fill(distance[3], 12960000);
+        Arrays.fill(distance[4], 12960000);
+        Arrays.fill(distance[5], 12960000);
+        Arrays.fill(distance[6], 12960000);
+        Arrays.fill(distance[7], 12960000);
+        Arrays.fill(distance[8], 12960000);
+
+        int visionLength = (int)(Math.sqrt(rc.getType().visionRadiusSquared) + 0.00001);
+        int queueStart = 0, queueEnd = 0;
+        queue[queueEnd++] = curLoc;
+        distance[visionLength][visionLength] = 0;
+        startingDir[visionLength][visionLength] = Direction.CENTER;
+
+        closestDir = curLoc.directionTo(target);
+
+        int minX = curLoc.x - visionLength, minY = curLoc.y - visionLength;
+        int closestDistance = 12960000;
+
+        int x, y, nx, ny, distanceRemaining, dist;   // declare once at top to save bytecode
+        Direction dir, curDir;
+        while (queueStart < queueEnd && Clock.getBytecodesLeft() >= bytecodeLimit) {  // one iteration can take around 600 bytecodes
+            curLoc = queue[queueStart++];
+            x = curLoc.x - minX; y = curLoc.y - minY;
+
+            distanceRemaining = Math.max(
+                    Math.abs(target.x - curLoc.x),
+                    Math.abs(target.y - curLoc.y)
+            ) * 5;
+
+            dist = distance[x][y];
+            curDir = startingDir[x][y];
+
+            if (closestDistance > dist + distanceRemaining) {
+                closestDistance = dist + distanceRemaining;
+                closestDir = curDir;
+            }
+
+            dir = Direction.NORTHWEST;
+            nextLoc = curLoc.add(dir);
+            nx = nextLoc.x - minX; ny = nextLoc.y - minY;
+            if (rc.canSenseLocation(nextLoc) && distance[nx][ny] == 12960000 && rc.sensePassability(nextLoc)) {
+                // check if we're processing starting location and trying to move to adjacent
+                if (curDir != Direction.CENTER) {
+                    startingDir[nx][ny] = curDir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                } else if (rc.canMove(dir)) {
+                    startingDir[nx][ny] = dir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                }
+            }
+
+            dir = Direction.WEST;
+            nextLoc = curLoc.add(dir);
+            nx = nextLoc.x - minX; ny = nextLoc.y - minY;
+            if (rc.canSenseLocation(nextLoc) && distance[nx][ny] == 12960000 && rc.sensePassability(nextLoc)) {
+                // check if we're processing starting location and trying to move to adjacent
+                if (curDir != Direction.CENTER) {
+                    startingDir[nx][ny] = curDir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                } else if (rc.canMove(dir)) {
+                    startingDir[nx][ny] = dir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                }
+            }
+
+            dir = Direction.SOUTHWEST;
+            nextLoc = curLoc.add(dir);
+            nx = nextLoc.x - minX; ny = nextLoc.y - minY;
+            if (rc.canSenseLocation(nextLoc) && distance[nx][ny] == 12960000 && rc.sensePassability(nextLoc)) {
+                // check if we're processing starting location and trying to move to adjacent
+                if (curDir != Direction.CENTER) {
+                    startingDir[nx][ny] = curDir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                } else if (rc.canMove(dir)) {
+                    startingDir[nx][ny] = dir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                }
+            }
+
+            dir = Direction.SOUTH;
+            nextLoc = curLoc.add(dir);
+            nx = nextLoc.x - minX; ny = nextLoc.y - minY;
+            if (rc.canSenseLocation(nextLoc) && distance[nx][ny] == 12960000 && rc.sensePassability(nextLoc)) {
+                // check if we're processing starting location and trying to move to adjacent
+                if (curDir != Direction.CENTER) {
+                    startingDir[nx][ny] = curDir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                } else if (rc.canMove(dir)) {
+                    startingDir[nx][ny] = dir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                }
+            }
+
+            dir = Direction.SOUTHEAST;
+            nextLoc = curLoc.add(dir);
+            nx = nextLoc.x - minX; ny = nextLoc.y - minY;
+            if (rc.canSenseLocation(nextLoc) && distance[nx][ny] == 12960000 && rc.sensePassability(nextLoc)) {
+                // check if we're processing starting location and trying to move to adjacent
+                if (curDir != Direction.CENTER) {
+                    startingDir[nx][ny] = curDir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                } else if (rc.canMove(dir)) {
+                    startingDir[nx][ny] = dir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                }
+            }
+
+            dir = Direction.EAST;
+            nextLoc = curLoc.add(dir);
+            nx = nextLoc.x - minX; ny = nextLoc.y - minY;
+            if (rc.canSenseLocation(nextLoc) && distance[nx][ny] == 12960000 && rc.sensePassability(nextLoc)) {
+                // check if we're processing starting location and trying to move to adjacent
+                if (curDir != Direction.CENTER) {
+                    startingDir[nx][ny] = curDir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                } else if (rc.canMove(dir)) {
+                    startingDir[nx][ny] = dir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                }
+            }
+
+            dir = Direction.NORTHEAST;
+            nextLoc = curLoc.add(dir);
+            nx = nextLoc.x - minX; ny = nextLoc.y - minY;
+            if (rc.canSenseLocation(nextLoc) && distance[nx][ny] == 12960000 && rc.sensePassability(nextLoc)) {
+                // check if we're processing starting location and trying to move to adjacent
+                if (curDir != Direction.CENTER) {
+                    startingDir[nx][ny] = curDir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                } else if (rc.canMove(dir)) {
+                    startingDir[nx][ny] = dir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                }
+            }
+
+            dir = Direction.NORTH;
+            nextLoc = curLoc.add(dir);
+            nx = nextLoc.x - minX; ny = nextLoc.y - minY;
+            if (rc.canSenseLocation(nextLoc) && distance[nx][ny] == 12960000 && rc.sensePassability(nextLoc)) {
+                // check if we're processing starting location and trying to move to adjacent
+                if (curDir != Direction.CENTER) {
+                    startingDir[nx][ny] = curDir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                } else if (rc.canMove(dir)) {
+                    startingDir[nx][ny] = dir; distance[nx][ny] = dist + 1; queue[queueEnd++] = nextLoc;
+                }
+            }
+        }
+        return closestDir;
+    }
+
+    public static Direction spreadOut(RobotController rc, float weightX, float weightY, SpreadSettings settings) throws GameActionException {
+        MapLocation currentLocation = rc.getLocation();
+        float x = currentLocation.x, y = currentLocation.y;
+        // push away from edges
+        weightX += (Math.max(0, cube(6 - x)) - Math.max(0, cube(x - (rc.getMapWidth() - 7)))) / 10f;
+        weightY += (Math.max(0, cube(6 - y)) - Math.max(0, cube(y - (rc.getMapHeight() - 7)))) / 10f;
+
+        RobotInfo[] robots = rc.senseNearbyRobots(settings.ally_dist_cutoff, rc.getTeam());
+        float dist;
+        float allyWeightX = 0, allyWeightY = 0;
+        MapLocation loc;
+        boolean notAnchor = settings != SpreadSettings.CARRIER_ANCHOR;
+        for (int i = robots.length; i --> 0;) {
+            // when spreading out anchoring carriers, we only care about other anchor carriers
+            if (rc.getType() == robots[i].getType() && (notAnchor || robots[i].getNumAnchors(Anchor.STANDARD) > 0)) {
+                loc = robots[i].location;
+                // add one to avoid div by 0 when running out of bytecode
+                dist = currentLocation.distanceSquaredTo(loc) + 1;
+                // subtract since we want to move away
+                allyWeightX -= (loc.x - x) / dist;
+                allyWeightY -= (loc.y - y) / dist;
+            }
+        }
+        weightX += allyWeightX * settings.ally_dist_factor;
+        weightY += allyWeightY * settings.ally_dist_factor;
+
+        // searching carriers should avoid enemy sightings
+        if (settings == SpreadSettings.CARRIER_SEARCHING) {
+            float enemyWeightX = 0, enemyWeightY = 0;
+            EnemySighting[] sightings = Comms.getEnemySightings(rc);
+            for (int i = sightings.length; i --> 0;) {
+                loc = sightings[i].location;
+                dist = currentLocation.distanceSquaredTo(loc) + 1;
+                enemyWeightX -= (loc.x - x) / dist;
+                enemyWeightY -= (loc.y - y) / dist;
+            }
+            weightX += enemyWeightX * 40;
+            weightY += enemyWeightY * 40;
+        }
+
+        int finalDx = FastRandom.nextInt(settings.random_bound) - settings.random_cutoff > weightX ? -1 : 1;
+        int finalDy = FastRandom.nextInt(settings.random_bound) - settings.random_cutoff > weightY ? -1 : 1;
+        return similarDirectionNoCloud(rc, new MapLocation(0, 0).directionTo(new MapLocation(finalDx, finalDy)));
+    }
+}
